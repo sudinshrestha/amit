8ad38824f98c3d6c0c03bbf7c2ba138139327ef6
sudinshrestha <sudins.shrestha@gmail.com>
2021-09-19 11:41:41 +1000

    map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
+import GetName from './components/GetName';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React rr
-        </a>
-      </header>
-    </div>
-  );
+
+
+const name=[
+  {
+  name: "sudin",
+  coin: "fantom" 
+  },
+  {
+    name: "amit",
+    coin: "solano"
+  },
+  {
+    name: "sasankh",
+    coin: "ethereum"
+  },
+  {
+    name: "dipesh",
+    coin: "cardano"
+  }
+]
+
+return(
+  <div>
+    {name.map((item)=>{
+      return  <GetName name={item}/>
+    })}
+</div>
+);
+
 }
+
 
 export default App;
